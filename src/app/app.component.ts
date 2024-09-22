3bd8ddc9b36317863801f08718da03e36080c8aa
MelihcanSrky <sarikayamelihcan-01@outlook.com>
2024-09-22 17:51:43 +0300

    crashcourse part 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, FormsModule, NgIf, NgFor, NgTemplateOutlet],
+  imports: [
+    RouterOutlet,
+    AppNavbar,
+    FormsModule,
+    NgIf,
+    NgFor,
+    NgTemplateOutlet,
+    NgSwitch,
+    NgSwitchCase,
+    NgStyle,
+    NgClass
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -26,23 +37,25 @@ export class AppComponent {
   //   console.log(this.loginCount);
   // }
 
-  users: Array<string> = ['John', 'Sam', 'Smith', 'Melih'];
-  usersObj: Array<any> = [
-    { id: 1, name: 'Melih' },
-    { id: 2, name: 'Can' },
-    { id: 3, name: 'Sari' },
-    { id: 4, name: 'Kaya' },
-  ];
-
-  addNewUser() {
-    let user = {
-      id: 5,
-      name: 'User 1'
-    };
-    this.usersObj.push(user);
-  }
-
-  onDelete(index: number) {
-    this.usersObj.splice(index, 1);
-  }
+  // users: Array<string> = ['John', 'Sam', 'Smith', 'Melih'];
+  // usersObj: Array<any> = [];
+  //
+  // constructor() {
+  //   console.log(this.usersObj.length);
+  // }
+  //
+  // addNewUser() {
+  //   let user = {
+  //     id: 5,
+  //     name: 'User 1'
+  //   };
+  //   this.usersObj.push(user);
+  // }
+  //
+  // onDelete(index: number) {
+  //   this.usersObj.splice(index, 1);
+  // }
+  //
+  // usersRole: string = 'Admin';
+  isLoggedIn: boolean = true;
 }
